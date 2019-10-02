ec4824d3ede7f1b870510777499d547c9e241185
eoleahy <eoin.97@live.ie>
2019-10-02 21:17:57 +0100

    cleaned up code. improved weather api call to get info for different locations

--- server.js
+++ server.js
@@ -4,23 +4,31 @@ const express = require("express");
 const request = require("request");
 const fs = require("fs");
 const path = require("path");
+
 const app = express();
-
 const port = 3000;
-let publicPath = path.resolve(__dirname,"public");
-//app.use("/static",express.static(publicPath))
+let publicPath = path.resolve(__dirname, "public");
 
-const API_KEY = JSON.parse(fs.readFileSync('config.json'));
-console.log(API_KEY);
+let key = JSON.parse(fs.readFileSync('config.json'));
+const API_KEY = key["api-key"];
+const weatherUrl = "https://api.openweathermap.org/data/2.5/forecast";
 
 
-app.get('/', (req, res) => res.sendFile(publicPath+"/client.html"));
 
-app.listen(port, () => console.log('Example app listening on port ${port}!'));
+app.get('/', (req, res) => res.sendFile(publicPath + "/client.html"));
 
-/*Put a promise in here
-request("https://api.openweathermap.org/data/2.5/forecast?q=Dublin,Ireland&APPID=3e2d927d4f28b456c6bc662f34350957", {json:true}, (err,res,body) =>{
-    if(err){ return console.log(err);}
-    console.log(res.body);
-});
-*/
+app.listen(port, () => console.log(`To view webpage visit localhost:${port}`));
+
+let getWeather = (city, country) =>{
+    let reqStr = `${weatherUrl}?q=${city},${country}&APPID=${API_KEY}`;
+    //console.log(reqStr);
+
+    request(reqStr, {json:true}, (err, res, body) => {
+        if(err){
+            return console.log(err);
+        }
+        console.log(JSON.stringify(res.body, null, 2));
+    });
+}
+
+//getWeather("Dublin", "Ireland");
