58793d0070d25e251e01b12c731cd7a555ea1171
eoleahy <eoin.97@live.ie>
2019-10-02 16:26:37 +0100

    added packages -added temporary HTTP request

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 "use strict";
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const request = require("request");
+const app = express();
+const port = 3000;
 
-const path = require("path")
+const path = require("path");
 
-let publicPath = path.resolve(__dirname,"public")
+let publicPath = path.resolve(__dirname,"public");
 //app.use("/static",express.static(publicPath))
 
 
-app.get('/', (req, res) => res.sendFile(publicPath+"/client.html"))
+app.get('/', (req, res) => res.sendFile(publicPath+"/client.html"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log('Example app listening on port ${port}!'));
+
+//Put a promise in here
+request("https://api.openweathermap.org/data/2.5/forecast?q=Dublin,Ireland&APPID=3e2d927d4f28b456c6bc662f34350957", {json:true}, (err,res,body) =>{
+    if(err){ return console.log(err);}
+    console.log(res.body);
+});
