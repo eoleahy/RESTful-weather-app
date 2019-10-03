7194cc0feefb87db1de4f28485d8a7d7cfbce1df
eoleahy <eoin.97@live.ie>
2019-10-03 14:13:25 +0100

    main page is linked to api call for dublin

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 let publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
 
 let key = JSON.parse(fs.readFileSync('config.json'));
 const API_KEY = key["api-key"];
@@ -16,19 +17,26 @@ const weatherUrl = "https://api.openweathermap.org/data/2.5/forecast";
 
 
 app.get('/', (req, res) => res.sendFile(publicPath + "/client.html"));
+app.get('/weather/:location',  getWeather);
 
 app.listen(port, () => console.log(`To view webpage visit localhost:${port}`));
 
-let getWeather = (location) =>{
-    let reqStr = `${weatherUrl}?q=${location}&APPID=${API_KEY}`;
-    //console.log(reqStr);
+function getWeather(req, res){
+    let loc = req.params['location'];
+    //console.log(loc);
+    if(loc === ""){
+        res.status(400);
+        res.send("Error 400: Bad request");
+        return;
+    }
 
-    request(reqStr, {json:true}, (err, res, body) => {
-        if(err){
-            return console.log(err);
-        }
-        console.log(JSON.stringify(res.body, null, 2));
+    let reqStr = `${weatherUrl}?q=${loc}&APPID=${API_KEY}`; 
+    request(reqStr, {json:true}, (err, apiResponse, body) => {
+        if(err){ return console.log(err); }
+
+        //console.log(JSON.stringify(response.body, null, 2));
+        res.send(apiResponse.body);
     });
 }
 
-getWeather("Dublin,Ireland");
+//getWeather("Dublin,Ireland");
