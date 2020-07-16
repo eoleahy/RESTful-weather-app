2ed51f1f32b6393eb016fbc5629922f1595803c3
eoleahy <eoin.97@live.ie>
2020-07-16 14:03:47 +0100

    removing node_modules

---
+++ app/server.js
@@ -0,0 +1,40 @@
+"use strict";
+const express = require("express");
+const path = require("path");
+const fetch = require("node-fetch");
+
+require('dotenv').config();
+
+const API_KEY = process.env.WEATHER_API;
+const app = express();
+
+const port = 3000;
+let publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+
+const weatherUrl = "https://api.openweathermap.org/data/2.5/forecast";
+
+app.get('/:location', sendWeather);
+app.get('/', (req, res) => res.sendFile(publicPath + "/client.html"));
+//app.get('/:location',  getWeather);
+
+
+app.listen(port, () => console.log(`To view webpage visit localhost:${port}`));
+
+function sendWeather(req, res) {
+    let loc = req.params['location'];
+    //console.log(loc);
+    let reqStr = `${weatherUrl}?q=${loc}&APPID=${API_KEY}&units=metric`;
+
+    let p = fetch(reqStr)
+    p.then(res => res.json())
+        .then(data => {
+            res.send({
+                data
+            });
+        })
+        .catch(err => {
+            res.send("Error 400: Bad request");
+        });
+}
